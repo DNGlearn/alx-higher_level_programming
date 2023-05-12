68cafd016b23382115bbe101bfd4c20b3da1796a
DNGlearn <dennis.gitau.learn@gmail.com>
2023-05-11 17:52:22 -0700

    konbini

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the number of and list of args."""
-    
+    """Print the number of and list of args.""" 
     import sys
 
     count = len(sys.argv) - 1
